ff109a011e2d1c2d08ab1e82917d15d153cfef10
tincann <saabnl@gmail.com>
2017-12-06 20:45:58 +0100

    Add project files.

---
+++ MortyNet.DigitRecognition/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MortyNet.DigitRecognition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
