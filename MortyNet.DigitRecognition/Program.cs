a945fdc01067bece56455dd446bc6cc384552b63
tincann <saabnl@gmail.com>
2017-12-06 22:15:28 +0100

    Refactor Sample visualization

--- MortyNet.DigitRecognition/Program.cs
+++ MortyNet.DigitRecognition/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using MortyNet.DigitRecognition.Data;
-using SixLabors.ImageSharp;
+using MortyNet.DigitRecognition.Visualization;
 
 namespace MortyNet.DigitRecognition
 {
@@ -10,27 +9,18 @@ namespace MortyNet.DigitRecognition
     {
         static void Main(string[] args)
         {
-	        var datasetDirectory = Path.Combine(Directory.GetCurrentDirectory(), "dataset");
+	        var datasetDirectory = "dataset";
 	        var trainingImagesPath = Path.Combine(datasetDirectory, "train-images.idx3-ubyte");
 	        var trainingLabelsPath = Path.Combine(datasetDirectory, "train-labels.idx1-ubyte");
 
 	        var trainingSet = MNISTLoader.LoadImageSamples(trainingImagesPath, trainingLabelsPath).Take(5);
-
-
+			
 	        var i = 0;
 	        foreach (var sample in trainingSet)
 	        {
-		        var pixels = sample.Data.Select(x => new Rgba32(x, x, x));
-		        var image = Image.LoadPixelData(pixels.ToArray(), sample.DataColumns, sample.DataRows);
-
-		        var path = Path.Combine("dataset", "examples", $"{i}_{sample.Label}.png");
-		        Directory.CreateDirectory(Path.GetDirectoryName(path));
-				using (var fs = File.OpenWrite(path))
-		        {
-					image.SaveAsPng(fs);
-				}
-
-				i++;
+			    var path = Path.Combine(datasetDirectory, "examples", $"{i}_{sample.Label}.png");
+				SampleVisualizer.SaveAsPng(sample, path);
+		        i++;
 	        }
 		}
     }
---
+++ MortyNet.DigitRecognition/Visualization/SampleVisualizer.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Linq;
+using MortyNet.DigitRecognition.Data;
+using SixLabors.ImageSharp;
+
+namespace MortyNet.DigitRecognition.Visualization
+{
+    public class SampleVisualizer
+    {
+	    public static void SaveAsPng(ImageSample sample, string path)
+	    {
+			var pixels = sample.Data.Select(x => new Rgba32(x, x, x)).ToArray();
+		    var image = Image.LoadPixelData(pixels, sample.DataColumns, sample.DataRows);
+
+		    Directory.CreateDirectory(Path.GetDirectoryName(path));
+		    using (var fs = File.OpenWrite(path))
+		    {
+			    image.SaveAsPng(fs);
+		    }
+		}
+    }
+}
